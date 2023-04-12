135c59e3542ddbc02bceff3dfc7cbfab5df2cfac
soumisamanta <soumisamanta1998@gmail.com>
2023-04-13 01:34:55 +0530

    merge conflicts

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-=======
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
->>>>>>> 1a3fa94338c3318084ada6bb5d9dc7f4e56084d7
   expect(linkElement).toBeInTheDocument();
 });
